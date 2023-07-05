16ea8972e95308afd1429d441b60abb4d6a9d8d8
carlDguez19 <carldominguez19@gmail.com>
2023-07-05 13:57:35 -0600

    pressing the add button now sets animation in motion to show form to user to fill out for new book. I will now create a new animation to remove form when user clicks cancel on the form or clicks the overlay(background)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //need to create a flip switch for the read variable to place on card
+let bookArr = [];
+let overlay = document.querySelector(".overlay");
+const newBookForm = document.querySelector(".bookForm");
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -7,4 +11,14 @@ function Book(title, author, pages, read){
     this.bookInfo = function() {
         return `"${this.title}" by ${this.author} \n${this.pages}.pgs`
     }
+}
+
+//add event listener on li click to bring up new book form
+const addBookButton = document.querySelector("li");
+addBookButton.addEventListener("click", newBookAnimation);
+
+function newBookAnimation(){
+    overlay.style.visibility = 'visible';
+    overlay.style.animation = 'increaseOpacity 1.5s forwards';
+    newBookForm.style.animation = 'formSlideTop 1.5s forwards';
 }
